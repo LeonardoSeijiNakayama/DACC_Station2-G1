98267863f79d929a8bef70e7ed2ecc244b4ee0e5
Caique-Afk <caique4dante@gmail.com>
2026-02-13 18:38:23 -0400

    Mecanica de subir escadas e as escadas

--- player.gd
+++ player.gd
@@ -3,27 +3,50 @@ extends CharacterBody2D
 
 var SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
 var DASH_SPEED = 1200
+
 var IS_DASHING = false
+var esta_na_escada = false
+var escalando = false
+
 
 func _physics_process(delta: float) -> void:
 	# Adiciona a gravidade
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Mexe com o pulo
-	if Input.is_action_pressed("Cima") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	
-	# Cuida do dash e corrida
+
+	# Cuida do estado de dash
 	if Input.is_action_just_pressed("Dash") and is_on_floor():
 		IS_DASHING = true
 		$Dash_timer.start()
-		
-	# Cuida da movimentação base do jogador
+
+
+	# Mexe com o pulo
+	if Input.is_action_pressed("Pulo") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+
+	# Lida com as escadas
+	if esta_na_escada and Input.is_action_just_pressed("Cima"):
+		escalando = true
+	if Input.is_action_just_pressed("Pulo") or esta_na_escada == false:
+		escalando = false
+	
+	if escalando == true:
+		var direction_escada := Input.get_axis("Cima", "Baixo")
+		if direction_escada:
+			if IS_DASHING:
+				velocity.y = direction_escada * DASH_SPEED
+			else:
+				velocity.y = direction_escada * SPEED
+		else:
+			velocity.y = move_toward(velocity.y, 0, SPEED)
+
+
+	# Cuida da movimentação base do jogador e dash
 	var direction := Input.get_axis("Esquerda", "Direita")
-	if direction:
+	if direction and escalando == false:
 		if IS_DASHING:
 			velocity.x = direction * DASH_SPEED
 		else:
@@ -31,7 +54,7 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
-	# Cuida do dash e corrida
+	# Cuida da corrida
 	if Input.is_action_pressed("Dash") and IS_DASHING == false:
 		SPEED = 600
 	else:
@@ -42,3 +65,13 @@ func _physics_process(delta: float) -> void:
 
 func _on_timer_timeout() -> void:
 	IS_DASHING = false
+
+
+func _on_escada_body_entered(body: Node2D) -> void:
+	esta_na_escada = true 
+	print("Entrou na escada")
+
+
+func _on_escada_body_exited(body: Node2D) -> void:
+	esta_na_escada = false
+	print("saiu da escada")
