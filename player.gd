b604b20344a2f925b9ce7e219b1ea6f57824f7f3
Caique-Afk <caique4dante@gmail.com>
2026-02-12 23:09:43 -0400

    Codei a movimentação basica do player e adcionei a mecanica de dash e corrida

--- player.gd
+++ player.gd
@@ -1,11 +1,44 @@
 extends CharacterBody2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+var DASH_SPEED = 1200
+var IS_DASHING = false
+
+func _physics_process(delta: float) -> void:
+	# Adiciona a gravidade
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Mexe com o pulo
+	if Input.is_action_pressed("Cima") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	# Cuida do dash e corrida
+	if Input.is_action_just_pressed("Dash") and is_on_floor():
+		IS_DASHING = true
+		$Dash_timer.start()
+		
+	# Cuida da movimentação base do jogador
+	var direction := Input.get_axis("Esquerda", "Direita")
+	if direction:
+		if IS_DASHING:
+			velocity.x = direction * DASH_SPEED
+		else:
+			velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	# Cuida do dash e corrida
+	if Input.is_action_pressed("Dash") and IS_DASHING == false:
+		SPEED = 600
+	else:
+		SPEED = 300
+	
+	move_and_slide()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_timer_timeout() -> void:
+	IS_DASHING = false
