36d4abc9ec8f1c4620b95a6a584c04936d42815b
Leonardo <leonardonakayama99@gmail.com>
2026-05-29 09:43:22 -0400

    feat: criei a cena de teste dos tilesets e background

---
+++ Tileset/tileset_test.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@export var train_scene: PackedScene = preload("res://Tileset/MariaFumaça/maria-fumaça.tscn")
+@export var spawn_position: Vector2 = Vector2(722, 166)
+
+@onready var timer: Timer = $TrainTimer
+
+func _ready() -> void:
+	timer.timeout.connect(spawn_train)
+	timer.start()
+
+func spawn_train() -> void:
+	var train:Train = train_scene.instantiate()
+	train.z_index = 1
+	train.position = spawn_position
+	get_parent().add_child(train)
